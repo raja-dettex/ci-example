99f1aba05fb339dabe7e677ad63b6ea2cdb59bb1
raja-dettex <rk6091039@gmail.com>
2024-03-16 13:19:15 +0530

    initial commit

---
+++ src/test/java/com/example/ciexample2/CiExample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ciexample2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiExample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
