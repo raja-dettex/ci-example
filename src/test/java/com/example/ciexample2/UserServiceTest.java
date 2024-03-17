7e7db5ca5cde6bebd254a3912253455bf8d91cd1
raja-dettex <rk6091039@gmail.com>
2024-03-17 11:05:55 +0530

    added one e2e test

--- src/test/java/com/example/ciexample2/UserServiceTest.java
+++ src/test/java/com/example/ciexample2/UserServiceTest.java
@@ -5,9 +5,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.util.Assert;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -21,7 +27,10 @@ public class UserServiceTest {
 
     @Test
     public void findall_test_positive() {
-        var users = this.service.getAll();
-        System.out.println(users);
+        var users  = IntStream.rangeClosed(1,5).mapToObj(i -> new User(i, "name" + i)).collect(Collectors.toList());
+        Mockito.when(mockedRepo.findAll()).thenReturn(users);
+        var result = this.service.getAll();
+        System.out.println(result.toString());
+        assert result == users;
     }
 }
